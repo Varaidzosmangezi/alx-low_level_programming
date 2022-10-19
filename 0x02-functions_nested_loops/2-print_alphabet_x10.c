cb1e17dd7bac2cb5b6cd46882252711d17982c6e
Varaidzosmangezi <mangezivaraidzos@gmail.com>
2022-10-18 17:51:53 -0700

    task0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * 2-print_alphabet_x10 - print alphabet times 10
+ *
+ * Description: print alphabet times 10
+ */
+
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+	char letter;
+
+	while (counter++ <= 9)
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	_putchar('\n');
+	return (0);
+}
