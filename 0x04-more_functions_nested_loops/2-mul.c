0427dfdafd9fd64fd32be26476121d6dc7af320f
Varaidzosmangezi <mangezivaraidzos@gmail.com>
2022-10-20 18:12:58 -0700

    task1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 intergers
+ * Description multiply 2 intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: The result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
