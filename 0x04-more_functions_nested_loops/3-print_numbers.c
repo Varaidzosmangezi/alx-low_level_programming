d32996b1cd9306e506d1bb06388c9c03d3579deb
Varaidzosmangezi <mangezivaraidzos@gmail.com>
2022-10-20 18:40:32 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,10 +8,11 @@
 
 void print_numbers(void)
 {
-	int num;
+	char c;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
-
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
